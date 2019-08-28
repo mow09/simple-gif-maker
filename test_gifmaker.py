60f157ec73bbca0289358a80840973dbcd026f05
mow09 <donj31313@gmail.com>
2019-08-28 13:13:02 +0200

    corrected names 2

--- test_gifmaker.py
+++ test_gifmaker.py
@@ -6,10 +6,10 @@ from gif_maker import gif_maker
 
 def test_making_a_gif_out_of_png():
     """Just reads in PNGs and makes a GIF out of it."""
-    FILEPATH = 'pics/'
-    gif_maker(FILEPATH, gif_name='test')
+    file_path = 'pics/'
+    gif_maker(file_path, gif_name='test')
     # created?
-    assert os.path.isfile(FILEPATH + 'test.gif')
-    os.remove(FILEPATH + 'test.gif')
+    assert os.path.isfile(file_path + 'test.gif')
+    os.remove(file_path + 'test.gif')
     # deleted?
-    assert not os.path.isfile(FILEPATH + 'test.gif')
+    assert not os.path.isfile(file_path + 'test.gif')
